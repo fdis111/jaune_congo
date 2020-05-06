04fb5dce464024839286dfcd56a3c7ec45214f84
fdis111 <disubif@gmail.com>
2020-05-06 13:39:04 +0200

    fin du header

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ejs = require("ejs-blocks");
-const sql = require("./config/db_config")
+//const sql = require("./config/db_config")
 const port = 8000;
 const app = express();
 
@@ -20,16 +20,17 @@ app.use(express.static('public'))
 
 
 app.get("/", (req, res) => {
-    sql.query(`select * from produits`,  (error , result) => {
-        if (error)
-        {
-            throw error;
-        }
-        else
-        {
-            res.render("index", {title: "Jaune congo", produits: result});
-        }
-    })
+    // sql.query(`select * from produits`,  (error , result) => {
+    //     if (error)
+    //     {
+    //         throw error;
+    //     }
+    //     else
+    //     {
+            //res.render("index", {title: "Jaune congo", produits: result});
+            res.render("index", {title: "Jaune congo"})
+      //  }
+    //})
 
     
 })
